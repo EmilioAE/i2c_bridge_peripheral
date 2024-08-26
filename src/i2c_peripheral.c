d83c531bbd47ce725b540a4941bf84356234c72e
Emilio Aguila Escalante <emilio@prettysmartlabs.com>
2024-08-26 12:11:35 -0600

    Initial commit

---
+++ src/i2c_peripheral.c
@@ -0,0 +1,130 @@
+#include <errno.h>
+#include <string.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/linker/devicetree_regions.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pinctrl.h>
+
+#include <zephyr/drivers/i2c.h>
+#include <nrfx_twis.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(i2c_peripheral);
+
+#include "crypto.h"
+#include "i2c_registers.h"
+
+#if CONFIG_NRFX_TWIS1
+#define I2C_S_INSTANCE 1
+#elif CONFIG_NRFX_TWIS2
+#define I2C_S_INSTANCE 2
+#elif CONFIG_NRFX_TWIS22
+#define I2C_S_INSTANCE 22
+#elif CONFIG_NRFX_TWIS131
+#define I2C_S_INSTANCE 131
+#else
+#error "TWIS instance not enabled or not supported"
+#endif
+
+#define NODE_TWIS   DT_NODELABEL(twi_peripheral)
+
+#define TWIS_MEMORY_SECTION                                                                    
+	COND_CODE_1(DT_NODE_HAS_PROP(NODE_TWIS, memory_regions),                                   \
+		    (__attribute__((__section__(                                                       \
+			    LINKER_DT_NODE_REGION_NAME(DT_PHANDLE(NODE_TWIS, memory_regions)))))),         \
+		    ())
+
+static const nrfx_twis_t twis = NRFX_TWIS_INSTANCE(I2C_S_INSTANCE);
+
+#define RX_BUFFER_SIZE 256
+static uint8_t i2c_rx_buffer[RX_BUFFER_SIZE] TWIS_MEMORY_SECTION;
+
+K_MSGQ_DEFINE(i2c_msgq, sizeof(struct i2c_reg_packet), 16, 1);
+
+enum i2c_cmds cmd_idx = I2C_REG_STATUS;
+void i2c_peripheral_handler(nrfx_twis_evt_t const *p_event)
+{
+	struct i2c_reg_enc_packet ciphertext;
+	struct i2c_reg_packet plaintext;
+
+	switch (p_event->type) {
+	case NRFX_TWIS_EVT_READ_REQ:
+		LOG_DBG("Read req");
+
+		plaintext.magic = I2C_PACKET_MAGIC;
+		plaintext.reg = cmd_idx;
+		memcpy(plaintext.plaintext, command_registers[cmd_idx].data, DATA_SIZE_BYTES);
+
+		encrypt_i2c_packet(&plaintext, &ciphertext);
+		nrfx_twis_tx_prepare(&twis, &ciphertext.data, I2C_PACKET_SIZE_BYTES);
+		break;
+
+	case NRFX_TWIS_EVT_READ_DONE:
+		LOG_DBG("Read done");
+		break;
+
+	case NRFX_TWIS_EVT_WRITE_REQ:
+		LOG_DBG("Write req");
+		memset(i2c_rx_buffer, 0, RX_BUFFER_SIZE);
+		nrfx_twis_rx_prepare(&twis, i2c_rx_buffer, RX_BUFFER_SIZE);
+		break;
+
+	case NRFX_TWIS_EVT_WRITE_DONE:
+		LOG_DBG("Write done");
+		// If the received data is in the proper amount...
+		if (p_event->data.rx_amount == sizeof(struct i2c_reg_enc_packet)) {
+			LOG_HEXDUMP_DBG(i2c_rx_buffer, I2C_PACKET_SIZE_BYTES, "i2c rx");
+			decrypt_i2c_packet(&plaintext, (struct i2c_reg_enc_packet *)i2c_rx_buffer);
+			// If our magic value is decrypted correctly...
+			if (plaintext.magic == I2C_PACKET_MAGIC) {
+				cmd_idx = plaintext.reg;
+				// If the command index is valid...
+				if (cmd_idx < I2C_REG_END) {
+					cmd_idx = plaintext.reg;
+					k_msgq_put(&i2c_msgq, &plaintext, K_NO_WAIT);
+				} else {
+					LOG_ERR("Invalid command index: %d", cmd_idx);
+				}
+			} else {
+				cmd_idx = I2C_REG_STATUS;
+				LOG_ERR("Invalid I2C packet magic");
+			}
+		} else {
+			LOG_ERR("Invalid encrypted packet length: %d", p_event->data.rx_amount);
+		}
+		break;
+
+	default:
+		LOG_DBG("TWIS event: %d\n", p_event->type);
+		break;
+	}
+}
+
+static int i2c_peripheral_init(void)
+{
+	const nrfx_twis_config_t config = {
+		.addr = {0x54, 0x00},
+		.skip_gpio_cfg = true,
+		.skip_psel_cfg = true,
+	};
+	int ret;
+
+	ret = nrfx_twis_init(&twis, &config, i2c_peripheral_handler);
+	if (ret != NRFX_SUCCESS) {
+		LOG_ERR("Init failed: %d", ret);
+		return -1;
+	}
+
+	PINCTRL_DT_DEFINE(NODE_TWIS);
+	ret = pinctrl_apply_state(PINCTRL_DT_DEV_CONFIG_GET(NODE_TWIS), PINCTRL_STATE_DEFAULT);
+
+	IRQ_CONNECT(DT_IRQN(NODE_TWIS), DT_IRQ(NODE_TWIS, priority),
+		    NRFX_TWIS_INST_HANDLER_GET(I2C_S_INSTANCE), NULL, 0);
+
+	nrfx_twis_enable(&twis);
+	LOG_INF("Enabled TWIS");
+	return 0;
+}
+
+SYS_INIT(i2c_peripheral_init, APPLICATION, 90);
---
+++ src/i2c_registers.c
@@ -0,0 +1,179 @@
+#include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#include "crypto.h"
+#include "i2c_registers.h"
+#include "i2c_packet.h"
+#include "ble_peripheral.h"
+#include "ble_packet.h"
+
+LOG_MODULE_REGISTER(i2c_registers);
+
+static const struct gpio_dt_spec data_ready = 
+	GPIO_DT_SPEC_GET(DT_NODELABEL(data_ready), gpios);
+
+extern struct k_msgq i2c_msgq;
+
+struct i2c_cmd_reg command_registers[I2C_REG_END] = {
+	{ /* I2C_REG_STATUS */
+		.data = {0x13, 0x37},
+		.read = true,
+		.write = false,
+	},
+	{ /* I2C_REG_DEV_ID */
+		.data = {0xBE, 0xEF},
+		.read = true,
+		.write = false,
+	}, 
+	{ /* I2C_REG_ADVERTISING */
+		.data = {},
+		.read = true,
+		.write = true,
+	}, 
+	{ /* I2C_REG_CONN_PARAM */
+		.data = {},
+		.read = true,
+		.write = true,
+	}, 
+	{ /* I2C_REG_CONN_POWER */
+		.data = {},
+		.read = true,
+		.write = true,
+	}, 
+	{ /* I2C_REG_TX_BUF */
+		.data = {},
+		.read = true,
+		.write = true,
+	}, 
+	{ /* I2C_REG_RX_BUF */
+		.data = {},
+		.read = true,
+		.write = true,
+	}, 
+};
+
+static int i2c_reg_status_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("Status register packet");
+	return 0;
+}
+
+static int i2c_reg_dev_id_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("Device ID packet");
+	return 0;
+}
+
+static int i2c_reg_advertising_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("Advertising packet");
+	if (packet->read) {
+		return 0;
+	}
+	return 0;
+}
+
+static int i2c_reg_connection_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("Connection packet");
+	if (packet->read) {
+		return 0;
+	}
+	return 0;
+}
+
+static int i2c_reg_power_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("Power packet");
+	if (packet->read) {
+		return 0;
+	}
+	return 0;
+}
+
+static int i2c_reg_tx_buf_handler(struct i2c_reg_packet *packet) {
+	LOG_INF("TX buf packet");
+	if (packet->read) {
+		return 0;
+	} else {
+		memcpy(command_registers[I2C_REG_TX_BUF].data, packet->plaintext, sizeof(packet->plaintext));
+
+		struct ble_packet plaintext;
+		memcpy(plaintext.plaintext, packet->plaintext, sizeof(packet->plaintext));
+		plaintext.magic = BLE_PACKET_MAGIC;
+
+		struct ble_enc_packet ciphertext;
+		encrypt_ble_packet(&plaintext, &ciphertext);
+		i2c_bridge_transmit(&ciphertext);
+		LOG_HEXDUMP_INF(plaintext.plaintext, sizeof(plaintext.plaintext), "Plaintext: ");
+		LOG_HEXDUMP_INF(ciphertext.data, sizeof(ciphertext.data), "Ciphertext: ");
+		LOG_INF("Sending to BLE central...");
+	}
+	return 0;
+}
+
+static int i2c_reg_rx_buf_handler(struct i2c_reg_packet *packet) {
+	if (packet->read) {
+		int err = gpio_pin_set_dt(&data_ready, 0);
+		if (err < 0) {
+			LOG_ERR("Error setting interrupt pin (err: %d)", err);
+		}
+		return 0;
+	}
+	return 0;
+}
+
+static int (*i2c_reg_handlers[])(struct i2c_reg_packet *) = {
+    i2c_reg_status_handler,
+    i2c_reg_dev_id_handler,
+    i2c_reg_advertising_handler,
+	i2c_reg_connection_handler,
+	i2c_reg_power_handler,
+    i2c_reg_tx_buf_handler,
+    i2c_reg_rx_buf_handler,
+};
+
+static bool is_read_command(uint8_t *data) {
+	for (int i = 0; i < DATA_SIZE_BYTES; i++) {
+	  if (data[i] != 0) {
+	  	return false;
+	  }
+	}
+	return true;
+}
+
+
+int i2c_register_handler(void) {
+	LOG_INF("Beginning secure I2C -> NUS bridge");
+
+	if (!gpio_is_ready_dt(&data_ready)) {
+		LOG_ERR("Interrupt GPIO not ready.");
+		return -1;
+	}
+
+	int err = gpio_pin_configure_dt(&data_ready, GPIO_OUTPUT_INACTIVE);
+	if (err < 0) {
+		LOG_ERR("Interrupt GPIO configuration failed (err: %d)", err);
+		return -1;
+	}
+
+	struct i2c_reg_packet plaintext;
+	while (true) {
+		k_msgq_get(&i2c_msgq, &plaintext, K_FOREVER);
+		plaintext.read = is_read_command(plaintext.plaintext);
+		i2c_reg_handlers[plaintext.reg](&plaintext);
+		LOG_HEXDUMP_DBG(plaintext.plaintext, sizeof(plaintext.plaintext), "i2c cmd");
+	}
+}
+
+void write_rx_register(uint8_t *buf) {
+	memcpy(command_registers[I2C_REG_RX_BUF].data, buf, DATA_SIZE_BYTES);
+	int err = gpio_pin_set_dt(&data_ready, 1);
+	if (err < 0) {
+		LOG_ERR("Error setting interrupt pin (err: %d)", err);
+	}
+}
+
+#define THREAD_STACK_SIZE 2048 
+#define THREAD_PRIORITY 5
+
+K_THREAD_DEFINE(i2c_register_thread, THREAD_STACK_SIZE,
+                i2c_register_handler, NULL, NULL, NULL,
+                THREAD_PRIORITY, 0, 0);
